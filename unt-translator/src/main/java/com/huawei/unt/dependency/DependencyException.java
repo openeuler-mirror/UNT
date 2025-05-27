5a44bbdccc7b78fce95911bf7b5d9fe84fdedae4
guojunfei <970763131@qq.com>
2025-05-27 20:09:17 +0800

    fix clean code issues in dependency and loader module

--- unt-translator/src/main/java/com/huawei/unt/dependency/DependencyException.java
+++
@@ -1,7 +0,0 @@
-package com.huawei.unt.dependency;
-
-public class DependencyException extends RuntimeException {
-    public DependencyException(String message) {
-        super(message);
-    }
-}
