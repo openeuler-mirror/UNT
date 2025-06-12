31ea21170a13b59d95fc462b5158125f02945875
yufengyu <yuyufeng1@huawei.com>
2025-06-11 22:13:23 -0400

    clean code Signed-off-by: yufengyu<yuyufeng1@huawei.com>

--- unt-translator/src/test/java/com/huawei/unt/translator/JavaClassTranslatorTest.java
+++ unt-translator/src/test/java/com/huawei/unt/translator/JavaClassTranslatorTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+
 package com.huawei.unt.translator;
 
 import com.huawei.unt.BaseTest;
@@ -5,35 +9,32 @@ import com.huawei.unt.dependency.DependencyAnalyzer;
 import com.huawei.unt.loader.JarHandler;
 import com.huawei.unt.model.JavaClass;
 import com.huawei.unt.type.NoneUDF;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sootup.core.inputlocation.AnalysisInputLocation;
-import sootup.core.model.SourceType;
 import sootup.java.bytecode.frontend.inputlocation.OTFCompileAnalysisInputLocation;
-import sootup.java.bytecode.frontend.inputlocation.PathBasedAnalysisInputLocation;
 import sootup.java.core.JavaSootClass;
 import sootup.java.core.views.JavaView;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.StringJoiner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class JavaClassTranslatorTest extends BaseTest{
+/**
+ * Test JavaClassTranslatorTest
+ *
+ * @since 2025-06-11
+ */
+public class JavaClassTranslatorTest extends BaseTest {
     @Test
     public void testInheritanceTranslate() throws IOException {
-
-        byte[] bytes = Files.readAllBytes(Paths.get("src/test/resources/translator/expect/TestInheritance.txt"));
-        String expectString = new String(bytes, StandardCharsets.UTF_8);
-
-        AnalysisInputLocation inputLocation = new OTFCompileAnalysisInputLocation(Paths.get("src/test/resources/translator/source/TestInheritance.java"));
+        AnalysisInputLocation inputLocation = new OTFCompileAnalysisInputLocation(
+                Paths.get("src/test/resources/translator/source/TestInheritance.java"));
         JavaView view = new JavaView(inputLocation);
         JarHandler jarHandler = new JarHandler(view);
         ArrayList<JavaClass> allClasses = new ArrayList<>();
@@ -54,6 +55,8 @@ public class JavaClassTranslatorTest extends BaseTest{
         }
         List<String> actual = translatorResults.get(0);
         String actualString = actual.toString();
+        byte[] bytes = Files.readAllBytes(Paths.get("src/test/resources/translator/expect/TestInheritance.txt"));
+        String expectString = new String(bytes, StandardCharsets.UTF_8);
         Assertions.assertTrue(expectString.equals(actualString));
     }
 
@@ -84,7 +87,6 @@ public class JavaClassTranslatorTest extends BaseTest{
         }
         List<String> actual = translatorResults.get(0);
         String actualString = actual.toString();
-//        System.out.println(actualString);
         System.out.println(actualString);
         System.out.println(expectString);
         Assertions.assertTrue(expectString.equals(actualString));
@@ -117,7 +119,6 @@ public class JavaClassTranslatorTest extends BaseTest{
         }
         List<String> actual = translatorResults.get(0);
         String actualString = actual.toString();
-//        System.out.println(actualString);
         Assertions.assertTrue(expectString.equals(actualString));
     }
 
--- unt-translator/src/test/java/com/huawei/unt/translator/TranslatorUtilsTest.java
+++ unt-translator/src/test/java/com/huawei/unt/translator/TranslatorUtilsTest.java
@@ -1,12 +1,22 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+
 package com.huawei.unt.translator;
 
 import com.huawei.unt.BaseTest;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test TranslatorUtilsTest
+ *
+ * @since 2025-06-11
+ */
 public class TranslatorUtilsTest extends BaseTest {
     @Test
-    public void testGetJarHashPath(){
+    public void testGetJarHashPath() {
         String expectHash = "22c6db8cf7db96d80649af5e38d9ca1fd3292dbe545d2f0c36a7e8c69826dc5f";
         String jarPath = new String("src/test/resources/loader/binary/loaderTestcase.jar");
         String actualHash = TranslatorUtils.getJarHashPath(jarPath);
