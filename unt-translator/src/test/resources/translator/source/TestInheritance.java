68ba35e8f1111cf712c6b3bdd9b67b336263b7b4
yufengyu <yuyufeng1@huawei.com>
2025-06-12 03:26:07 +0000

    clean code in TestInheritance.java.
    Signed-off-by: yufengyu <yuyufeng1@huawei.com>

--- unt-translator/src/test/resources/translator/source/TestInheritance.java
+++ unt-translator/src/test/resources/translator/source/TestInheritance.java
@@ -1,6 +1,15 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+
 import org.apache.flink.api.common.functions.MapFunction;
 
-public class TestInheritance implements MapFunction<String,String> {
+/**
+ * Test TestInheritance
+ *
+ * @since 2025-06-11
+ */
+public class TestInheritance implements MapFunction<String, String> {
     @Override
     public String map(String s) throws Exception {
         return s.replace('.', '_');
